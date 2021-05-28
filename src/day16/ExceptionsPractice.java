e0a0aa5c142341c0720c65ceee30d83dd1aa5dfe
Taani Maama <taani1maama@live.com>
2021-05-28 13:09:28 -0700

    Added the finally clause.

--- src/day16/ExceptionsPractice.java
+++ src/day16/ExceptionsPractice.java
@@ -17,8 +17,8 @@ public class ExceptionsPractice {
             System.out.println("Element 6 at index 5 = " + c[5]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Exception thrown: " + e);
+        } finally {
+            System.out.println("Finished try-catch. finally cause");
         }
-
-        System.out.println("Finished try-catch.");
     }
 }
