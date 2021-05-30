3e2b4f9e8abbd674d9eb475dc28c2d73d467808e
Taani Maama <taani1maama@live.com>
2021-05-30 09:05:42 -0700

    Added the finally clause.

--- src/day16/ExceptionsPractice.java
+++ src/day16/ExceptionsPractice.java
@@ -12,13 +12,23 @@ public class ExceptionsPractice {
         // System.out.println(intArray[10]);
         // The 10th index doesn't actually exists and so this is the unchecked exception.
 
+//        try {
+//            int[] c = new int[5];
+//            System.out.println("Element 6 at index 5 = " + c[5]);
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Exception thrown: " + e);
+//        } finally {
+//            System.out.println("Finished try-catch. finally cause");
+//        }
+
+        String S = "Hello";
+
+        int intFromStr = Integer.parseInt(S);
+
         try {
-            int[] c = new int[5];
-            System.out.println("Element 6 at index 5 = " + c[5]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Exception thrown: " + e);
-        } finally {
-            System.out.println("Finished try-catch. finally cause");
+            System.out.println(intFromStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Bad String");
         }
     }
 }
